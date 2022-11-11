5d3466abd1928f43d00a445cf0acacb33fc1d5c6
Tawee Drake <113539642+taweedrake@users.noreply.github.com>
2022-11-11 18:51:46 +0300

    trying

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 let showIcon = document.getElementById('show');
 let hideIcone = document.getElementById('hide');
 let nav = document.getElementById('nav');
+let image = document.getElementById('scroll');
+let i;
+i = 0;
+let images = new Array(
+    "images/h.jpg",
+    "images/hmh.jpg",
+    "images/hmh.jpeg"
+);
+let len = images.length;
 function show() {
     nav.style.display = 'block'; 
     showIcon.style.display = 'none';
@@ -14,5 +23,15 @@ function hide() {
 function play() {
     let video = document.querySelector('#logo');
     video.play();
+}
+
+function scroll() {
+    if (i > len - 1) {
+        i=0
+    }
+    image.src = images[i];
+    i++;
+
+    setTimeout("scroll()", 10000);
 }
 
